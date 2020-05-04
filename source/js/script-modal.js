14f1c4c8cb12f7a268af2b1dca442304a1e1f930
Laura0120 <gabrielyanlaura@gmail.com>
2020-05-04 22:45:12 +0300

    Производительность вёрстки

--- source/js/script-modal.js
+++ source/js/script-modal.js
@@ -1,15 +1,10 @@
-var buyButtons = document.querySelectorAll(".catalog-item__button");
+var buyButtons = document.querySelectorAll(".button--buy");
 var modal = document.querySelector(".modal");
 var form = modal.querySelector(".form-add-cart");
 var close = form.querySelector(".form-add-cart__button");
 
-var onCloseModal = (evt) => {
-  evt.preventDefault();
-  modal.classList.remove("modal--show");
-};
-
-buyButtons.forEach((buyButton) => {
-  buyButton.addEventListener("click", (evt) => {
+buyButtons.forEach(function (buyButton) {
+  buyButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.add("modal--show");
   });
@@ -17,21 +12,24 @@ buyButtons.forEach((buyButton) => {
 
 document.addEventListener(
   "click",
-  (evt) =>
+  function (evt) {
     !evt.target.closest(".form-add-cart") &&
-    !evt.target.closest(".catalog-item__button") &&
-    onCloseModal(evt),
+      !evt.target.closest(".button--buy") &&
+      modal.classList.remove("modal--show");
+  },
   false
 );
 
-close.addEventListener("click", onCloseModal);
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--show");
+});
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (modal.classList.contains("modal--show")) {
       modal.classList.remove("modal--show");
-      form.classList.remove("modal--show");
     }
   }
 });
